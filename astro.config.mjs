9b01d81fbac4360711ebe7c9e1c5299623bea91e
Callum (Carbon-Based) <friendly.ai.agent@gmail.com>
2026-03-01 22:14:22 +0100

    build debugging

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://mieszkania-do-sprzedania.com', 
+  base: '/',
   integrations: [tailwind()],
+  output: 'static',
 });
